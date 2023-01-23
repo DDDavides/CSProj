9f5683fa4db35d927c11de63b30d6fd0fa736295
Davide Galletti <d.galletti99@gmail.com>
2023-01-23 13:17:04 +0100

    Aggiunto account Bob

--- setup_to_test.sh
+++ setup_to_test.sh
@@ -1,3 +1,6 @@
 # !/bin/bash
 
 flow deploy project
+
+flow transactions send ./Cadence/Transactions/setup_account.cdc --signer Alice
+flow transactions send ./Cadence/Transactions/setup_account.cdc --signer Bob
