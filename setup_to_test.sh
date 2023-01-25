0f426a2671b2a9b924314db2168310a1adc006c6
Davide Galletti <d.galletti99@gmail.com>
2023-01-25 12:40:43 +0100

    Aggiunti script utili per il testing dei contratti e delle transazioni

--- setup_to_test.sh
+++ setup_to_test.sh
@@ -1,6 +1,27 @@
 # !/bin/bash
 
+transactions="./Cadence/Transactions"
+scripts="./Cadence/Scripts"
+
+
+## deploy dei contratti del progetto
+echo "Deploy dei contratti del progetto.."
 flow deploy project
 
-flow transactions send ./Cadence/Transactions/setup_account.cdc --signer Alice
-flow transactions send ./Cadence/Transactions/setup_account.cdc --signer Bob
+
+## Setup degli account
+echo "Setup degli account di test.."
+flow transactions send ${transactions}/setup_account.cdc --signer Alice
+flow transactions send ${transactions}/setup_account.cdc --signer Bob
+flow transactions send ${transactions}/setup_account.cdc
+
+
+## Mint di un NFT da parte dell'emulatore
+echo "\bMinting di un NFT a carico dell'emulatore.."
+flow transactions send ${transactions}/mint_nft.cdc \
+    f8d6e0586b0a20c7 Prova descrizione thumbnail '[1.0]' '["nulla"]'
+
+
+## Per vedere i metadati di un NFT dell'account dell'emulatore
+# id_nft=0
+# flow scripts execute ${scripts}/get_nft_metadata.cdc 0xf8d6e0586b0a20c7 $id_nft
